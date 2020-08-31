4b403b6df1818111114a7b662a56a768b60143bc
UrsKahmann <urs@smartmobilefactory.com>
2020-08-31 15:07:21 +0200

    disabled appcenter

--- AppCenter/AppCenterSDK+SMFLogger.swift
+++ AppCenter/AppCenterSDK+SMFLogger.swift
@@ -5,7 +5,7 @@
 //  Created by Konstantin Deichmann on 11.09.19.
 //  Copyright © 2019 SmartMobileFactory. All rights reserved.
 //
-#if os(iOS)
+/*
 import Foundation
 import AppCenter
 import AppCenterDistribute
@@ -109,4 +109,4 @@ extension AppCenterSDK {
 		}
 	}
 }
-#endif
+*/
