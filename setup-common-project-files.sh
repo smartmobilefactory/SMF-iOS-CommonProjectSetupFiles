53f835e0d57a7ce66d3cdb76ab2e59a65344a4a8
Kevin Delord <delord.kevin@gmail.com>
2020-09-17 12:14:31 +0200

    Deprecate unused arguments

--- setup-common-project-files.sh
+++ setup-common-project-files.sh
@@ -2,8 +2,9 @@
 # Calls all common project setup scripts. Exceptions can be enabled with flags.
 #
 # Author Hans Seiffert
+# Updated Kevin Delord
 #
-# Last revised 05/01/2017
+# Last revised 17/09/2020
 
 #
 # Constants
@@ -11,13 +12,15 @@
 
 readonly noSwiftlintFlag="--no-swiftlint"
 readonly noPRTemplateCopyFlag="--no-pr-template-copy"
-readonly noCodebeatFlag="--no-codebeat"
-readonly noCodeClimateFlag="--no-codeclimate"
 readonly noXcodeCheck="--no-xcodecheck"
-readonly buildConfigurationFlag="--buildconfig"
 readonly targetTypeFlag="--targettype"
 readonly breakingInternalFrameworkVersioningFlag="--use-breaking-internal-framework-versioning"
 readonly swiftUIFlag="--SwiftUI"
+
+readonly buildConfigurationFlag="--buildconfig" # deprecated
+readonly noCodebeatFlag="--no-codebeat" # deprecated
+readonly noCodeClimateFlag="--no-codeclimate" # deprecated
+
 
 readonly scriptBaseFolderPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -29,11 +32,8 @@ isFramework=false
 useBreakingInternalFrameworkVersioning=false
 copyPRTemplate=true
 callSwiftlint=true
-callCodebeat=true
-callCodeClimate=true
 checkXcodeVersion=true
 projectDir="$(pwd)"
-isDebugConfiguration=false
 isSwiftUIProject=false
 
 #
@@ -44,12 +44,11 @@ function display_usage () {
 	echo "This script performs all common project setup scripts by default. You can optionally pass the projects base directory path as argument. Exceptions can be declared with the flags:"
 	echo -e "$noSwiftlintFlag\t\t\t\t- Don't run swiftlint"
 	echo -e "$noPRTemplateCopyFlag\t\t\t- Don't copy the GitHub PR Template file"
-	echo -e "$noCodebeatFlag\t\t\t\t- Don't copy the default SMF codebeat configuration"
-	echo -e "$noCodeClimateFlag\t\t\t- Don't copy the default SMF Code Climate configuration"
 	echo -e "$breakingInternalFrameworkVersioningFlag\t -Use the \"BreakingInternal framework versioning system\" (only for frameworks)"
 	echo -e "$swiftUIFlag\t\t\t\t- Use custom SwiftLint rules for swift ui projects"
-	echo -e "\nUsage:\n$ $0 $noCodebeatFlag"
-	echo -e "or:\n$ $0 $noCodebeatFlag /Code/Project/Test"
+	echo -e "\nUsage:\n$ $0 $swiftUIFlag"
+	echo -e "or:\n$ $0 $swiftUIFlag /Code/Project/Test"
+	exit 1
 }
 
 #
@@ -58,11 +57,8 @@ function display_usage () {
 
 while test $# -gt 0; do
 	case "$1" in
-		$buildConfigurationFlag)
-			configName=$(echo "$2" | awk '{print tolower($0)}')
-			if [ $configName = "debug" ]; then
-				isDebugConfiguration=true
-			fi
+		$buildConfigurationFlag) # Deprecated parameter
+			# For legacy and retro-compatibility keep the 'switch-case' and pass through the arguments
 			shift
 			shift
 			# break
@@ -86,8 +82,8 @@ while test $# -gt 0; do
 			shift
 			# break
 			;;
-		$noCodebeatFlag)
-			callCodebeat=false
+		$noCodebeatFlag) # Deprecated parameter
+			# For legacy and retro-compatibility keep the 'switch-case' and pass through the arguments
 			shift
 			# break
 			;;
@@ -101,8 +97,8 @@ while test $# -gt 0; do
 			shift
 			# break
 			;;
-		$noCodeClimateFlag)
-			callCodeClimate=false
+		$noCodeClimateFlag) # Deprecated parameter
+			# For legacy and retro-compatibility keep the 'switch-case' and pass through the arguments
 			shift
 			# break
 			;;
@@ -132,14 +128,6 @@ cd "$scriptBaseFolderPath"
 
 if [ $callSwiftlint = true ]; then
 	./SwiftLint/copy-and-run-swiftlint-config.sh "$projectDir" $isFramework $isSwiftUIProject || exit 1;
-fi
-
-if [ $callCodebeat = true ]; then
-	./Codebeat/copy-codebeat-config.sh "$projectDir" || exit 1;
-fi
-
-if [ $callCodeClimate = true ]; then
-	./CodeClimate/copy-codeclimate-config.sh "$projectDir" || exit 1;
 fi
 
 if [ $copyPRTemplate = true ]; then
