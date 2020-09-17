a2411360ff75f6f223eaba1abe148fa9b1e3bafb
Kevin Delord <delord.kevin@gmail.com>
2020-09-17 18:25:51 +0200

    Remove old crappy code and rewrite logic to remove disabled custom rules from config file

--- SwiftLint/copy-and-run-swiftlint-config.sh
+++ SwiftLint/copy-and-run-swiftlint-config.sh
@@ -11,7 +11,8 @@
 #
 
 readonly scriptBaseFolderPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-readonly temporarySwiftLintConfigFilename=".$(uuidgen)-swiftlint.yml"
+readonly tmpFile=".$(uuidgen)-swiftlint.yml"
+readonly regexFirstOccurenceRuleName="([A-Za-z_]+).*"
 
 #
 # Variables
@@ -30,8 +31,14 @@ if [  -z "$1" ]; then
 	projectDir="$scriptBaseFolderPath"
 fi
 
-# Contains the local SwiftLint settings (.project-swiftlint.yml). Global so we dont read it multiple times.
+#
+# Global Arrays
+#
+
+# Contains the lines of a read file
 declare -a local_lines
+# Contains a list of disabled rules (strings)
+declare -a all_disabled_rules
 
 # File to array keeping newlines.
 function read_local_settings() {
@@ -43,15 +50,24 @@ function read_local_settings() {
 	done < $1
 }
 
-# Write the desired section of extra settings to the temporary file.
-function write_local_settings() {
+# Extract the values from a YML section.
+# Parameters:
+# $1: String representing the section to find adn extract the values from
+# $2: Action parameter: either "--write-to-file" or "--match-and-store-rule"
+# $3: Output file to write the data to. Only used if "--write-to-file" is provided.
+function extract_values_from_section() {
 	# Variable which is true if we are in the excluded section
 	local is_in_matched_section=false
 
 	# Section to look for
 	local matched_section=$1
 
+	# Prepare storage
+	unset all_disabled_rules
+	let i=0
+
 	for project_line in "${local_lines[@]}"; do
+
 		# Determine if we are in the excluded section
 		if [[ "$project_line" == "$matched_section" ]]; then
 			is_in_matched_section=true
@@ -60,18 +76,33 @@ function write_local_settings() {
 			is_in_matched_section=false
 		fi
 
-		# Copy the excluded path line if we are in the excluded section
+		# If the current line is within the matched section
 		if $is_in_matched_section; then
-			echo "$project_line" >> "$2"
+			if [[ "$2" == "--write-to-file" ]]; then
+				# And if we should write to an output file THEN echo the line
+				echo "$project_line" >> "$3"
+
+			elif [[ "$2" == "--match-and-store-rule" ]]; then
+				# And if we should match the rule and store it into an array.
+				if [[ $project_line =~ $regexFirstOccurenceRuleName ]]; then
+					disabled_rule="${BASH_REMATCH[1]}"
+					# Apply new format as to better identify the custom rules.
+					# The declaration of a custom rule is always "  name_of_rule:"
+					all_disabled_rules[i]="  $disabled_rule:"
+					((++i))
+					echo "Disabled rule: '$disabled_rule'"
+				fi
+			fi
 		fi
 	done
 }
 
-# Merge configuration files together
+# Merge swiftlint configuration files together
 # Parameters:
 # $1: The local/custom config file
 # $2: The base file in which the custom config should be merged into
-# $3: The output file with the merged configuration. Must not exist (or be empty).
+# $3: The action to do when extracting the rules. See parameters of the function "extract_values_from_section" for more info.
+# $4: The output file with the merged configuration. Must not exist (or be empty).
 function merge_swiftlint_configuration() {
 
 	read_local_settings "$1"
@@ -79,80 +110,56 @@ function merge_swiftlint_configuration() {
 	# Read the commons SwiftLint configuration file
 	while IFS= read -r global_line; do
 		# Copy the line first as this allows the script to directly copy the project depened paths if the excluded sections start was found
-		echo "$global_line" >> "$3"
+		echo "$global_line" >> "$4"
 		if ([[ "$global_line" =~ (^excluded:) ]] || [[ "$global_line" =~ (^disabled_rules:) ]] || [[ "$global_line" =~ (^opt_in_rules:) ]]) then
-			write_local_settings "$global_line" "$3"
+			extract_values_from_section "$global_line" "$3" "$4"
 		fi
 	done < "$2"
 }
 
-# Adds disabled rules to disabled section and removes custom rules for the custom rules section
-function add_disabled_rules_from_config_file() {
-	local config_file_yml=$1
-	local base_file=$2
-	disabled_pattern="disabled_rules:"
-	disabled_flag_1=false
-	disabled_flag_2=false
-	in_rule=false
-	temporarySwiftUILintConfigFilename=".$(uuidgen)-swiftlint-swift-ui.yml"
+# Delete the disabled custom rules from the configuration file.
+# Without this logic the rule would still work even though it is listed in the section "disabled_rules:".
+# Parameters:
+# $1: The base configuration file to remove the disabled custom rules from.
+function delete_custom_disabled_rules() {
 
+	# Read and cache the source file
+	read_local_settings "$1"
 
-	touch "$temporarySwiftUILintConfigFilename"
-
-	while IFS= read -r line_1 || [[ -n "$line_1" ]]; do
-
-		if [[ $disabled_flag_1 == true ]]; then
-
-			while IFS= read -r line_2 || [[ -n "$line_2" ]]; do
-
-				if [[ $disabled_flag_2 == true ]]; then
-					echo "$line_2" >> "$temporarySwiftUILintConfigFilename"
-				fi
-
-				if [[ $line_2 =~ $disabled_pattern ]]; then
-					disabled_flag_2=true
-				fi
-			done < "$config_file_yml"
-			disabled_flag_1=false
+	# Extract a list of all disabled rules
+	while IFS= read -r line; do
+		if ([[ "$line" =~ (^disabled_rules:) ]]) then
+			extract_values_from_section "$line" "--match-and-store-rule"
 		fi
+	done < "$1"
 
-		disabled_flag_2=false
+	# Create output file
+	outputFile="$1.clean"
+	touch "$outputFile"
 
-		if [[ $line_1 =~ $disabled_pattern ]]; then
-			disabled_flag_1=true
-			echo $line_1 >> "$temporarySwiftUILintConfigFilename"
-		else
-			while IFS= read -r line_2 || [[ -n "$line_2" ]]; do
-				if [[ $disabled_flag_2 == true ]]; then
-					if [[ $line_1 =~ ${line_2:3} ]]; then
-						in_rule=first
-					fi
-				fi
+	# Parse the source file and remove disabled custom rule
+	line_in_disabled_rule=false
+	while IFS= read -r line; do
+		# If the current line contains the custom rule declaration (with the special formating) then toggle the boolean.
+		if [[ " ${all_disabled_rules[@]} " =~ $line ]]; then
+			line_in_disabled_rule=true
+			echo "Disabled (and removed) custom rule: $line"
 
-				if [[ $line_2 =~ $disabled_pattern ]]; then
-					disabled_flag_2=true
-				fi
-			done < "$config_file_yml"
+		# If the current line is within a disabled custom rule then do not write to the new output file.
+		# The current line is not in the rule anymore WHEN it does not start with 3 whitespaces;
+		# in this case the line is a new rule declaration (that is not disabled).
+		elif ( [[ "$line_in_disabled_rule" = true ]] && [[ ! $line == "   "* ]] ); then
+			echo "$line" >> "$outputFile"
+			line_in_disabled_rule=false
 
-			if [[ $in_rule == false ]]; then
-				echo "$line_1" >> "$temporarySwiftUILintConfigFilename"
-			elif [[ $in_rule == first ]]; then
-				in_rule=true
-			elif [[ ${line_1:3:1} != " " ]]; then
-				echo "$line_1" >> "$temporarySwiftUILintConfigFilename"
-				in_rule=false
-			fi
-
-			if [[ $line_1 =~ $disabled_pattern ]]; then
-				disabled_flag_1=true
-			fi
+		# Finally, if the line is not within a rule, simply write to the output file
+		elif [[ "$line_in_disabled_rule" = false ]]; then
+			echo "$line" >> "$outputFile"
 		fi
+	done < "$1"
 
-	done < "$base_file"
-
-	cp "$temporarySwiftUILintConfigFilename" "$temporarySwiftLintConfigFilename"
-	rm "$temporarySwiftUILintConfigFilename"
-	return 0
+	# Replace the source file by the new output file
+	mv "$outputFile" "$1"
 }
 
 #
@@ -163,44 +170,40 @@ function add_disabled_rules_from_config_file() {
 cd "$scriptBaseFolderPath"
 
 # Copy the normal swiftlint file as temporary one (for later use in this script).
-cp "swiftlint.yml" "$temporarySwiftLintConfigFilename"
+cp "swiftlint.yml" "$tmpFile"
 
 # Merge the excluded paths of the commons and the project specific configuration
 if [ -f "$projectDir/.project-swiftlint.yml" ]; then
 	# Merge the project specific configuration.
-	merge_swiftlint_configuration "$projectDir/.project-swiftlint.yml" "$temporarySwiftLintConfigFilename" "$temporarySwiftLintConfigFilename.project"
-	mv "$temporarySwiftLintConfigFilename.project" "$temporarySwiftLintConfigFilename"
+	merge_swiftlint_configuration "$projectDir/.project-swiftlint.yml" "$tmpFile" "--write-to-file" "$tmpFile.project"
+	mv "$tmpFile.project" "$tmpFile"
 else
 	# Copy the project swiftlint configuration template to the repository.
 	cp "project-swiftlint.yml" "$projectDir/.project-swiftlint.yml"
 fi
 
-if [[ $swiftUI == true ]]; then
+if [[ $swiftUI = true ]]; then
 	# Merge the SwiftUI specific configuration.
-	merge_swiftlint_configuration "swiftlint+swiftUI.yml" "$temporarySwiftLintConfigFilename" "$temporarySwiftLintConfigFilename.swiftUI"
-	mv "$temporarySwiftLintConfigFilename.swiftUI" "$temporarySwiftLintConfigFilename"
-
-	# Disabled certain rules listet in swiftlint+swiftUI.yml if this is a swiftUI project
-	# TODO: delete custom rules?
-	# add_disabled_rules_from_config_file "swiftlint+swiftUI.yml" "$temporarySwiftLintConfigFilename"
+	merge_swiftlint_configuration "swiftlint+swiftUI.yml" "$tmpFile" "--write-to-file" "$tmpFile.swiftUI"
+	mv "$tmpFile.swiftUI" "$tmpFile"
 fi
 
 if [ $isFramework = true ]; then
 	# Merge the Frameworks specific configuration.
-	merge_swiftlint_configuration "swiftlint+frameworks.yml" "$temporarySwiftLintConfigFilename" "$temporarySwiftLintConfigFilename.frameworks"
-	mv "$temporarySwiftLintConfigFilename.frameworks" "$temporarySwiftLintConfigFilename"
-	# TODO: delete custom rules?
-	# add_disabled_rules_from_config_file "swiftlint+frameworks.yml" "$temporarySwiftLintConfigFilename"
+	merge_swiftlint_configuration "swiftlint+frameworks.yml" "$tmpFile" "--write-to-file" "$tmpFile.frameworks"
+	mv "$tmpFile.frameworks" "$tmpFile"
 fi
 
+# Delete the disabled custom rules from the configuration file
+delete_custom_disabled_rules "$tmpFile"
+
 # Copy the Swiftlint file to the projects base folder
-cp "$temporarySwiftLintConfigFilename" "$projectDir/.swiftlint.yml"
+cp "$tmpFile" "$projectDir/.swiftlint.yml"
 
 # Remove the temporary file after it was copied
-rm "$temporarySwiftLintConfigFilename"
+rm "$tmpFile"
 
 # Go to the project folder and run swiftlint from there
-
 cd "$projectDir"
 
 if [ -e "$projectDir/Pods/SwiftLint" ]; then
