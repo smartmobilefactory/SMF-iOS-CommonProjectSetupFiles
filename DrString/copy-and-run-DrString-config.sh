fe6786bae40371d4654b1a1bbf8f4e46b23764c3
Pierre Rothmaler <proth@users.noreply.github.com>
2020-09-23 14:56:25 +0200

    added DrString for framework projects

---
+++ DrString/copy-and-run-DrString-config.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Copies the DrString configuration to the projects base folder
+#
+# Author Pierre Rothmaler
+#
+# Last revised 23/09/2020
+
+#
+# Constants
+#
+
+readonly scriptBaseFolderPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+# Variables
+#
+
+projectDir="$1"
+
+#
+# Check requirements
+#
+
+# Check if project directory is provided. If not: Use the scripts base directory.
+if [  -z "$1" ]; then
+	projectDir="$scriptBaseFolderPath"
+fi
+
+#
+# Logic
+#
+
+# Go the folder which contains this script
+cd "$scriptBaseFolderPath"
+
+# Copy the DrString file to the projects base folder
+cp "drstring.toml" "$projectDir/.drstring.toml"
+
+# Go to the project folder and run DrString from there
+cd "$projectDir"
+
+DRSTRING_EXECUTABLE="$scriptBaseFolderPath/drstring"
+echo "FOUND PIERRE $DRSTRING_EXECUTABLE"
+echo "PIERRE AGAIN $projectDir/.drstring.toml"
+
+
+theDir="$( pwd )"
+
+echo "PIERRE? $theDir"
+
+if [ -f "$DRSTRING_EXECUTABLE" ]; then
+	"$DRSTRING_EXECUTABLE" check --config-file $projectDir/.drstring.toml || true
+else
+	echo "warning: DrString not installed."
+	exit 1
+fi
