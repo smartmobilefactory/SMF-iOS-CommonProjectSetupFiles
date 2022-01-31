4341d76c6efecbdfef550fd932e62fed6ac76a8e
Sanel Zsivics <sanel@smartmobilefactory.com>
2022-01-31 12:39:21 +0100

    Create common script for generating acknowledgements plist

---
+++ Strato/Licences.rb
@@ -0,0 +1,17 @@
+require 'fileutils'
+
+class Licences
+
+	def self.generateCleanAcknowledgementsPlist(target, path)
+		puts "Generating acknowledgements with dependencies from " + target.name
+
+		source = 'Pods/Target Support Files/' + target.name + '/' + target.name + '-acknowledgements.plist'
+		FileUtils.cp_r(source, path, :remove_destination => true)
+		
+		expand_path = File.expand_path(path)
+		
+		# See https://smartmobilefactory.atlassian.net/browse/STRFRAMEWORK-2640
+		`swift Submodules/SMF-iOS-CommonProjectSetupFiles/Strato/cleanLicences.swift #{expand_path}`
+	end
+	
+end
